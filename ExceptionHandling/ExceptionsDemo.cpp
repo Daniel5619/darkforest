55886707bf469e2a41107ef882bfc656572a18d7
Jean-Marc Raymond <jeanmarc_raymond@intuit.com>
2025-04-29 19:22:01 -0400

    simple exceptions demo

---
+++ ExceptionHandling/ExceptionsDemo.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdexcept> // Standard exception library
+
+class DanielException : public std::exception {
+    const char* what() const throw(){
+        return "custom error occured";
+    }
+};
+
+int division(int n, int d){
+    int returnVal;
+
+        if (d == 0) {
+            throw DanielException(); // Throwing an exception
+        }
+       returnVal = n/d;
+
+    return returnVal;
+}
+
+int main() {
+
+    int a = 10, b = 0;
+    
+    try {
+    
+        int result = division(a,b); 
+
+        std::cout << "that worked out just fine:" << result << std::endl;
+
+    }catch (const DanielException& e){
+        std::cout << "Some daniel specific thing" << std::endl;
+    }catch (const std::runtime_error& e) { // Catching an exception
+        std::cout << "Caught a runtime exception: " << e.what() << std::endl;    
+    }catch (const std::exception& e) { // Catching an exception
+      std::cout << "Caught an exception: " << e.what() << std::endl;
+    }
+
+
+    return 0;
+}
